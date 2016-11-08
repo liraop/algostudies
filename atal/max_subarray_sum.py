76460472eddcc86d67e49846e9c5142409d1a0e4
liraop <lirapdo@gmail.com>
2016-11-07 21:56:15 -0300

    maximum subarray sum added

---
+++ atal/max_subarray_sum.py
@@ -0,0 +1,32 @@
+def max_subarray_sum_trivial(a):
+    max_sum = 0
+
+    for i in range(0, len(a)):
+        for j in range(0, len(a)):
+            sum = 0
+            for k in range(i, j):
+                sum += a[k]
+                if sum > max_sum:
+                    max_sum = sum
+    print(max_sum)
+
+
+def max_subarray_sum_improved(a):
+    previous_sum = a[0]
+
+    for i in range(0, len(a)):
+        actual_sum = 0
+        for j in range(i, len(a)):
+            actual_sum += a[j]
+            if actual_sum >= previous_sum:
+                previous_sum = actual_sum
+            else:
+                i += 1
+
+    print(previous_sum)
+
+
+a = [1, -2, 1, 2, -5, -5, -6, 1]
+
+max_subarray_sum_trivial(a)
+max_subarray_sum_improved(a)
