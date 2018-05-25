afb1e9ac166af462d4b91de93821b90b65ae8b0a
= <=>
2018-05-24 21:41:01 -0300

    lista3 - primeira questao

---
+++ atal/bruteforce/compara_string.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+import unittest
+
+
+### Existem três tipos de edições que podem ser executadas em strings: inserir um caractere, remover um caractere ou substituir um caractere. Dadas duas strings, escreva uma função para verificar se uma string pode se igualar à outra usando no máximo uma edição. Resolva com força bruta. Analise o custo do algoritmo proposto.
+
+def comparaString(s1, s2):
+        diferenca = abs(len(s1)-len(s2))
+        if(diferenca > 1):
+            return False
+        else:
+            falta = 0
+            if (diferenca == 1): falta += 1
+ 
+            for i in range(0, min(len(s1),len(s2))):
+                if (s1[i] != s2[i]): falta += 1
+ 
+            return (falta <= 1)
+
+
+class Testes(unittest.TestCase):
+	def casoBase(self):
+		self.assertTrue(comparaString("pele","pele"))
+		self.assertFalse(comparaString("aheuash","aheuasheuas"))
+		
+	def casosPassa(self):
+		self.assertTrue(comparaString("pele","ple"))
+		self.assertTrue(comparaString("pele","bele"))
+
+	def casosFalha(self):
+		self.assertFalse(comparaString("pele","beke"))
+		self.assertFalse(comparaString("pExemplesiae","pele"))
+
+if __name__ == '__main__':
+    unittest.main()
